bb35e1b3a9337a8f646dffcc114564c67cc9778a
Francis <francisabonyi@gmail.com>
2021-09-30 11:18:16 +0100

    Getir test

--- src/test/records/app.spec.test.js
+++ src/test/records/app.spec.test.js
@@ -14,23 +14,41 @@ const url = "/api/v1";
 
 
   describe("it should make a Post Request", () => {
-  it("HTTP POST Request", async () => {
-    const result = await chai.request(server)
-      .post(`${url}/records`)
-      .set('Accept', 'application/json')
-      .send(payload)
-      const status = result.status
-      const { code, msg, records } = result.body
-      expect(status).to.equal(200)
-      expect(code).to.equal(0)
-      expect(msg).to.equal('success')
-      expect(records).to.contain.property('key')
-      expect(records).to.contain.property('createdAt')
-      expect(records).to.contain.property('totalCount')
-      // assert.equal(status, 200)
-      // assert.equal(code, 0)
-      // assert.equal(msg, 'success')
-      // assert.exists(records)
-  }).timeout(10000);
+    it("it should get the home page", (done) => {
+      chai
+        .request(server)
+        .post(`${url}/records`)
+       .set('Accept', 'application/json')
+       .send(payload)
+        .end((err, result) => {
+          const status = result.status
+          const { code, msg, records } = result.body
+          expect(status).to.equal(200)
+          expect(code).to.equal(0)
+          expect(msg).to.equal('success')
+          expect(records).to.contain.property('key')
+          expect(records).to.contain.property('createdAt')
+          expect(records).to.contain.property('totalCount')
+          done();
+        });
+    });
+  // it("HTTP POST Request", async () => {
+  //   const result = await chai.request(server)
+  //     .post(`${url}/records`)
+  //     .set('Accept', 'application/json')
+  //     .send(payload)
+  //     const status = result.status
+  //     const { code, msg, records } = result.body
+  //     expect(status).to.equal(200)
+  //     expect(code).to.equal(0)
+  //     expect(msg).to.equal('success')
+  //     expect(records).to.contain.property('key')
+  //     expect(records).to.contain.property('createdAt')
+  //     expect(records).to.contain.property('totalCount')
+  //     // assert.equal(status, 200)
+  //     // assert.equal(code, 0)
+  //     // assert.equal(msg, 'success')
+  //     // assert.exists(records)
+  // })
   });
 
