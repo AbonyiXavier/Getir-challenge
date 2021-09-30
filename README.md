55f3346e61908817359d9b0cc4316e91c8523300
Francis <francisabonyi@gmail.com>
2021-09-30 11:44:03 +0100

    Getir test

--- README.md
+++ README.md
@@ -1,3 +1,59 @@
 # Getir-challenge
 
 [![Build Status](https://app.travis-ci.com/AbonyiXavier/Gitr-challenge.svg?branch=main)](https://app.travis-ci.com/AbonyiXavier/Gitr-challenge)
+
+# Getting Started
+
+To obtain a copy of this app download or clone the repository at this [url](https://github.com/AbonyiXavier/Getir-challenge)
+
+Postman collection documentation link [url](https://documenter.getpostman.com/view/7775892/UUy1fnS8)
+
+# Prerequisites
+
+You must have
+
+- NodeJs Installed
+- A browser Installed
+- A REST API client(like POSTMAN) Installed
+- An Internet connection to download the dependencies.
+
+## Installing locally
+
+- (If the repository wasn't cloned)Extract the contents of the downloaded zip file into any suitable location on the computer
+- In the command prompt, cd to the root of the directory you extracted the app into
+- Run 'npm install' to install all dependencies
+- Run 'npm run dev' to start the application
+- Run 'npm run test' to run test on the application
+- In a browser address bar navigate to ''
+
+# Using Getir Challenge API through a restful client
+
+- Open any restful client application initially installed
+- Select the appropriate http method. POST
+
+
+- Use the POST method
+- Use this url link here
+
+- Test Data
+
+```Bash
+ {
+	"startDate": "2015-01-26",
+	"endDate": "2018-02-02",
+	"minCount": 2700,
+    "maxCount": 3000
+}
+
+## Built With
+
+- NodeJs
+- Express
+- Mongodb(database)
+- Mongoose (ODM)
+- Mocha and Chai
+- Travis CI
+
+## Author
+
+- AbonyiXavier
--- src/test/records/app.spec.test.js
+++ src/test/records/app.spec.test.js
@@ -1,10 +1,7 @@
 import chai, { expect } from "chai";
-import chatHttp from 'chai-http';
+import chatHttp from "chai-http";
 import server from "../../server";
 import { payload } from "./test.data";
-
-let assert = chai.assert;
-
 
 chai.should();
 
@@ -12,43 +9,23 @@ chai.use(chatHttp);
 
 const url = "/api/v1";
 
-
-  describe("it should make a Post Request", () => {
-    it("HTTP POST Request", (done) => {
-      chai
-        .request(server)
-        .post(`${url}/records`)
-       .set('Accept', 'application/json')
-       .send(payload)
-        .end((err, result) => {
-          const status = result.status
-          const { code, msg, records } = result.body
-          expect(status).to.equal(200)
-          expect(code).to.equal(0)
-          expect(msg).to.equal('success')
-          expect(records).to.contain.property('key')
-          expect(records).to.contain.property('createdAt')
-          expect(records).to.contain.property('totalCount')
-          done();
-        })
-    });
-  // it("HTTP POST Request", async () => {
-  //   const result = await chai.request(server)
-  //     .post(`${url}/records`)
-  //     .set('Accept', 'application/json')
-  //     .send(payload)
-  //     const status = result.status
-  //     const { code, msg, records } = result.body
-  //     expect(status).to.equal(200)
-  //     expect(code).to.equal(0)
-  //     expect(msg).to.equal('success')
-  //     expect(records).to.contain.property('key')
-  //     expect(records).to.contain.property('createdAt')
-  //     expect(records).to.contain.property('totalCount')
-  //     // assert.equal(status, 200)
-  //     // assert.equal(code, 0)
-  //     // assert.equal(msg, 'success')
-  //     // assert.exists(records)
-  // })
+describe("it should make a Post Request", () => {
+  it("HTTP POST Request", (done) => {
+    chai
+      .request(server)
+      .post(`${url}/records`)
+      .set("Accept", "application/json")
+      .send(payload)
+      .end((err, result) => {
+        const status = result.status;
+        const { code, msg, records } = result.body;
+        expect(status).to.equal(200);
+        expect(code).to.equal(0);
+        expect(msg).to.equal("success");
+        expect(records).to.contain.property("key");
+        expect(records).to.contain.property("createdAt");
+        expect(records).to.contain.property("totalCount");
+        done();
+      });
   });
-
+});
